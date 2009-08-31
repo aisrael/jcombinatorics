bbf355647e5e7d04c7a60104a808a4b971ed8fa0
AlistairIsrael <aisrael@gmail.com>
2009-08-31 10:39:09 +0800

    Added Javadoc for MathUtils.identityPermutation

--- src/main/java/jcombinatorics/util/MathUtils.java
+++ src/main/java/jcombinatorics/util/MathUtils.java
@@ -27,6 +27,10 @@ public final class MathUtils {
     }
 
     /**
+     * Fills the given array with a[i] = i, for example, if a.length = 4, then
+     * fills a with { 0, 1, 2, 3 }. Used throughout permutation and combination
+     * generation as the first result (lexicographically).
+     *
      * @param a
      *        an array
      */
