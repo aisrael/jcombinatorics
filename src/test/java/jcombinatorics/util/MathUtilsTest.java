e8da84466dbc272f88d98502cfa708060a575872
AlistairIsrael <aisrael@gmail.com>
2009-09-01 07:41:43 +0800

    Removed MathUtils#lcm(), gcd() and max() [not used]

--- src/test/java/jcombinatorics/util/MathUtilsTest.java
+++ src/test/java/jcombinatorics/util/MathUtilsTest.java
@@ -9,13 +9,10 @@
 package jcombinatorics.util;
 
 import static jcombinatorics.util.MathUtils.factorial;
-import static jcombinatorics.util.MathUtils.lcm;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import java.util.Arrays;
-
-import jcombinatorics.permutations.Permutations;
 
 import org.junit.Test;
 
@@ -68,49 +65,4 @@ public final class MathUtilsTest {
                     .toString(expected[i]), Arrays.toString(a)), expected[i], a);
         }
     }
-
-    /**
-     * Test for {@link MathUtils#max(int[])}.
-     */
-    @Test
-    public void testMax() {
-        assertEquals(1, MathUtils.max(1));
-        assertEquals(0, MathUtils.max(-1, 0));
-        assertEquals(1, MathUtils.max(Integer.MIN_VALUE, 1, 0));
-    }
-
-    /**
-     * Test for {@link MathUtils#lcm(int[])}.
-     */
-    @Test
-    public void testLcm() {
-        assertEquals(2, lcm(2));
-
-        final int[] expected = { 4, 6, 9, 18, 42, };
-        final int[] a = { 2, 2, 3, 6, 6, };
-        final int[] b = { 4, 3, 9, 9, 21, };
-        for (int i = 0; i < expected.length; ++i) {
-            assertEquals(expected[i], lcm(a[i], b[i]));
-        }
-        assertEquals(12, lcm(2, 3, 4));
-        assertEquals(60, lcm(2, 4, 3, 5, 6));
-        final int[] elements = { 2, 3, 4, 5, 6 };
-        for (final int[] numbers : Permutations.over(elements)) {
-            assertEquals("P(" + Arrays.toString(numbers) + ") should be 60", 60, MathUtils.lcm(numbers));
-        }
-    }
-
-    /**
-     * Test for {@link MathUtils#gcd(int, int)}.
-     */
-    @Test
-    public void testGcd() {
-        final int[] expected = { 3, 5, 1, 2, 3 };
-        final int[] a = { 3, 0, 9, 2, 6 };
-        final int[] b = { 0, 5, 28, 4, 9 };
-        for (int i = 0; i < expected.length; ++i) {
-            assertEquals(expected[i], MathUtils.gcd(a[i], b[i]));
-        }
-    }
-
 }
