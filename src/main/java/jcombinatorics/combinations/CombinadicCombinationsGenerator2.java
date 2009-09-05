0c659e5cfeb9f02318123216ff5de666e3e47fb3
Alistair A. Israel <aisrael@gmail.com>
2009-09-06 00:00:22 +0800

    Optimize, optimize

--- src/main/java/jcombinatorics/combinations/CombinadicCombinationsGenerator2.java
+++ src/main/java/jcombinatorics/combinations/CombinadicCombinationsGenerator2.java
@@ -137,19 +137,17 @@ public class CombinadicCombinationsGenerator2 implements Generator<int[]> {
         public int[] next() {
             int nn = n;
             long m = l - 1;
-            int kk = k;
-            for (int i = 0; i < k; ++i) {
+            for (int i = k; i > 0; --i) {
                 --nn;
                 // find largest v
                 int v = nn;
-                long c = count(v, kk);
+                long c = count(v, i);
                 while (c > m) {
-                    c = c * (v - kk) / v;
+                    c = c * (v - i) / v;
                     --v;
                 }
-                --kk;
                 m -= c;
-                a[i] = (n - 1) - v;
+                a[k - i] = (n - 1) - v;
             }
             --l;
             return a;
